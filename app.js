9b560b29c99ccf99ffbedb18bc2e20c8d0a13bed
leegeunhyeok <lghlove0509@naver.com>
2019-04-02 11:23:59 +0900

    UPDATE: Add koa-static, static.path config

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const serve = require('koa-static')
 const config = require('config')
 
 const app = new Koa()
 const router = new Router()
+
+app.use(serve(__dirname + config.get('static.path')))
 
 router.get('/', (ctx, next) => {
   ctx.body = 'home'
