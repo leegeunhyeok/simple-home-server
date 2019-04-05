1322ae096b7ec4da27f639f29b2ed9bd1d514593
leegeunhyeok <lghlove0509@naver.com>
2019-04-05 09:46:46 +0900

    UPDATE: Add proxy config

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 const Koa = require('koa')
 const Router = require('koa-router')
@@ -10,13 +11,16 @@ const app = new Koa()
 const router = new Router()
 
 const PORT = config.get('port')
+const PROXY = config.get('proxy')
 const PATH = config.get('static.path')
+const PUBLIC_PATH = path.join(__dirname, PATH)
+const INDEX_PATH = path.join(PUBLIC_PATH, config.get('static.index'))
 
-app.use(mount('/home', serve(__dirname + PATH)))
+app.use(mount(PROXY, serve(PUBLIC_PATH)))
 
-router.get('/home', (ctx) => {
+router.get(PROXY, (ctx) => {
   ctx.type = 'html'
-  ctx.body = fs.createReadStream(__dirname + PATH + '/index.html')
+  ctx.body = fs.createReadStream(INDEX_PATH)
 })
 
 app.use(router.routes())
