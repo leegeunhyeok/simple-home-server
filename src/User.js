c846006866a15268a29b2db6d872892f8018866f
leegeunhyeok <lghlove0509@naver.com>
2019-04-09 12:37:54 +0900

    ADD: User model

---
+++ src/User.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+const sequelize = require('./Database')
+
+const User = sequelize.define('User', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  ip: {
+    type: Sequelize.STRING(16),
+    allowNull: false
+  },
+  firstConnect: {
+    type: Sequelize.DATE,
+    default: new Date(),
+    allowNull: false
+  },
+  lastConnect: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  connectCount: {
+    type: Sequelize.INTEGER,
+    default: 1,
+    allowNull: false
+  }
+})
+
+export default User
