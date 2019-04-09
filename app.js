081ee2c5f9ff49284b5a7f1cebd0bf7f61fa62f6
leegeunhyeok <lghlove0509@naver.com>
2019-04-09 13:46:05 +0900

    UPDATE: Add router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 const Koa = require('koa')
+const Router = require('koa-router')
 const serve = require('koa-static')
 const mount = require('koa-mount')
 const config = require('config')
@@ -9,24 +10,33 @@ const config = require('config')
 const User = require('./src/User').model
 
 const app = new Koa()
+const router = new Router()
 
 const PORT = config.get('port')
-const PROXY = config.get('proxyPath')
+const ROOT = config.get('root')
 const PATH = config.get('static.path')
 const PUBLIC_PATH = path.join(__dirname, PATH)
 const INDEX_PATH = path.join(PUBLIC_PATH, config.get('static.index'))
 
 app.proxy = config.get('proxy')
-app.use(mount(PROXY, serve(PUBLIC_PATH)))
-app.use(async ctx => {
+app.use(mount(ROOT, serve(PUBLIC_PATH)))
+
+router.get(ROOT, async ctx => {
   try {
     await User.connect(ctx.request.ip)
   } catch (e) {
     console.error(`Error: ${e}`)
   }
-  ctx.set('Content-Type', 'text/html')
+  ctx.type = 'html'
   ctx.body = fs.createReadStream(INDEX_PATH)
 })
+
+router.all('*', ctx => {
+  ctx.redirect(ROOT)
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 app.listen(PORT, async () => {
   await User.sync()
