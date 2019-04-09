aeca015c2158360e9c8ce995ca90990b3f2ca482
leegeunhyeok <lghlove0509@naver.com>
2019-04-09 13:39:54 +0900

    UPDATE: Remove router

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const path = require('path')
 
 const Koa = require('koa')
-const Router = require('koa-router')
 const serve = require('koa-static')
 const mount = require('koa-mount')
 const config = require('config')
@@ -10,7 +9,6 @@ const config = require('config')
 const User = require('./src/User').model
 
 const app = new Koa()
-const router = new Router()
 
 const PORT = config.get('port')
 const PROXY = config.get('proxyPath')
@@ -20,23 +18,15 @@ const INDEX_PATH = path.join(PUBLIC_PATH, config.get('static.index'))
 
 app.proxy = config.get('proxy')
 app.use(mount(PROXY, serve(PUBLIC_PATH)))
-
-router.get(PROXY, async ctx => {
+app.use(async ctx => {
   try {
     await User.connect(ctx.request.ip)
   } catch (e) {
     console.error(`Error: ${e}`)
   }
-  ctx.type = 'html'
+  ctx.set('Content-Type', 'text/html')
   ctx.body = fs.createReadStream(INDEX_PATH)
 })
-
-router.all('*', ctx => {
-  ctx.redirect('/')
-})
-
-app.use(router.routes())
-app.use(router.allowedMethods())
 
 app.listen(PORT, async () => {
   await User.sync()
